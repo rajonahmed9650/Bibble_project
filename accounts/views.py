3159e6b0b2c6ade5793cd3f3ef3afbb7432e9e03
rajonahmed9650 <rajonahmed9650@gmail.com>
2026-01-03 20:56:17 +0600

    done

--- accounts/views.py
+++ accounts/views.py
@@ -56,7 +56,7 @@ class SignupView(APIView):
             user=user,
             package=Package.objects.filter(package_name="free").first(),
             current_plan="free",
-            expired_at=timezone.now() + timedelta(minutes =  10080),
+            expired_at=timezone.now() + timedelta(minutes =10080),
             is_active=True
         )
 
