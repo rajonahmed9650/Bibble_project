381b07a83fbc7afa652c35e43c9b083a126d97ae
rajonahmed9650 <rajonahmed9650@gmail.com>
2026-01-06 11:59:47 +0600

    ok

--- bibble_project/settings.py
+++ bibble_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^_ew)$#_y+o_i6cx@_=tqc&2_r=s*z%=du3w@f65eyl@95%=4b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     # "test3.fireai.agency",
